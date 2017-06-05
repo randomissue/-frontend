6445d17a2d4852b62b989a08e9305149de428c8f
Moritz Riede <mriede@tuta.io>
2017-06-06 01:08:51 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var menuOpened = false;
+
+window.onload = function() {
+  document.getElementById("issue-button").addEventListener("click", getIssue);
+}
+
+var getIssue = function() {
+  alert("Test");
+}
