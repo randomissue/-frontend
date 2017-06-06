f868e5485f3a989a1e8b1ba43263c73eb08e9800
Moritz Riede <mriede@tuta.io>
2017-06-06 06:02:45 +0200

    Added loading information

--- js/main.js
+++ js/main.js
@@ -2,21 +2,31 @@ var app = new Vue({
   el: "#app",
   data: {
     issueJSON: undefined,
-    showIssue: false
+    showIssue: false,
+    buttonText: this.buttonStates.normal
+    buttonStates: {
+      normal: "Get a random GitHub issue!",
+      loading: "Loading ...",
+      finished: "Finished!"
+    }
   },
   created: function() {
     this.getIssue();
   },
   methods: {
     getIssue: function() {
-      console.log("getIssue executed");
       var xhr = new XMLHttpRequest();
       xhr.open("GET", "/issue", true);
       xhr.onload = function (e) {
+        if (xhr.readyState === 0) {
+          this.buttonText = this.buttonStates.loading;
+        }
         if (xhr.readyState === 4) {
           if (xhr.status === 200) {
+            this.buttonText = "Parsing JSON ...";
             this.issueJSON = JSON.parse(xhr.responseText);
             console.log(this.issueJSON.title);
+            this.buttonText = this.buttonState.finished
             this.showIssue = true;
           } else {
             console.error(xhr.statusText);
