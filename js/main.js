7ef87f28314567e7d0081308b0d931d34752261e
Moritz Riede <mriede@tuta.io>
2017-06-06 02:09:16 +0200

    Rewrote functions in VueJS

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
-var menuOpened = false;
-
-window.onload = function() {
-  document.getElementById("issue-button").addEventListener("click", getIssue);
-}
-
-var getIssue = function() {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "/issue", true);
-  xhr.onload = function (e) {
-  if (xhr.readyState === 4) {
-    if (xhr.status === 200) {
-      console.log(xhr.responseText);
-    } else {
-      console.error(xhr.statusText);
+var app = new Vue({
+  el: "body",
+  functions: {
+    getIssue: function() {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", "/issue", true);
+      xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            renderIssue(xhr.responseText);
+          } else {
+            console.error(xhr.statusText);
+          }
+        }
+      };
+      xhr.onerror = function (e) {
+        console.error(xhr.statusText);
+      }
+      xhr.send(null);
     }
   }
-};
-xhr.onerror = function (e) {
-  console.error(xhr.statusText);
-};
-xhr.send(null);
+});
+
+
+var renderIssue = function(issueJSON) {
+  console.log(issueJSON);
 }
